4347f0f76a6d33bf98069dc9f3cdfa75145c016e
christaopher <hiimleetao@gmail.com>
2022-04-08 15:55:44 +0800

    help design and some functions

--- app/src/main/java/pk/name/myapplication/helpFragment.java
+++ app/src/main/java/pk/name/myapplication/helpFragment.java
@@ -3,17 +3,22 @@ package pk.name.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import pk.name.myapplication.databinding.FragmentHelpBinding;
 import pk.name.myapplication.databinding.FragmentHomeBinding;
 
 
-public class helpFragment extends Fragment {
+public class helpFragment extends Fragment implements View.OnClickListener {
     private FragmentHelpBinding binding;
+
+    Button help_back;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +26,27 @@ public class helpFragment extends Fragment {
         binding = FragmentHelpBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         // Inflate the layout for this fragment
+
+        TextView FAQ;
+
+        help_back = root.findViewById(R.id.help_back);
+        FAQ = root.findViewById(R.id.FAQ);
+        FAQ.setText(getResources().getString(R.string.FAQ1));
         return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.help_back:
+                ChangeFragment(new menuFragment());
+                break;
+        }
+    }
+
+    private void ChangeFragment (Fragment fragment){
+        FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment, fragment);
+        fragmentTransaction.commit();
     }
 }
