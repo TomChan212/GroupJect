b004cc5a69061cceded72972cdc27b5b19eebf5f
skitlol <s01030612@gmail.com>
2022-04-25 18:47:25 +0800

    update foodplanner

--- app/src/main/java/pk/name/myapplication/foodplannerFragment.java
+++ app/src/main/java/pk/name/myapplication/foodplannerFragment.java
@@ -96,117 +96,117 @@ public class foodplannerFragment extends Fragment implements View.OnTouchListene
             tv_suggest.setText("Suggested calories per day: 2000-2200 kcal");
             switch (day){
                 case 1:
-                    tv_breakfast2.setText("3 onion stuffed parantha + 1 cup curd + 3 cashews + 4 almonds + 2 walnuts");
-                    tv_lunch2.setText("1 cup moong dal/ chicken curry + 1 cup potato and caulifllower vegetable + 3 chapatti + 1/2 cup rice + salad");
-                    tv_dinner2.setText("1 cup beans potato vegetable + 3 chapatti + salad");
+                    tv_breakfast2.setText("3 onion stuffed parantha\n1 cup curd\n3 cashews\n4 almonds\n2 walnuts");
+                    tv_lunch2.setText("1 cup moong dal/ chicken curry\n1 cup potato and caulifllower vegetable\n3 chapatti\n1/2 cup rice\nsalad");
+                    tv_dinner2.setText("1 cup beans potato vegetable\n3 chapatti\nsalad");
                     break;
                 case 2:
-                    tv_breakfast2.setText("3 paneer stuffed besan cheela + green chutney + 1 cup curd + 3 cashews + 4 almonds + 2 walnuts");
-                    tv_lunch2.setText("1 cup masoor dal + 1 cup calocasia + 3 chapatti + 1/2 cup rice + 1 cup low curd + salad");
-                    tv_dinner2.setText("1 cup carrot peas vegetable +3 chapatti + salad");
+                    tv_breakfast2.setText("3 paneer stuffed besan cheela\ngreen chutney\n1 cup curd\n3 cashews\n4 almonds\n2 walnuts");
+                    tv_lunch2.setText("1 cup masoor dal\n1 cup calocasia\n3 chapatti\n1/2 cup rice\n1 cup low curd\nsalad");
+                    tv_dinner2.setText("1 cup carrot peas vegetable\n3 chapatti\nsalad");
                     break;
                 case 3:
-                    tv_breakfast2.setText("1.5 cup vegetable bread upma + 1 cup milk + 3 cashews + 4 almonds + 2 walnuts");
-                    tv_lunch2.setText("1 cup rajma curry + 1 cup spinach potato + 3 chapatti + 1/2 cup rice + salad");
-                    tv_dinner2.setText("1.5 cup parwal vegetable + 3 chapatti + salad");
+                    tv_breakfast2.setText("1.5 cup vegetable bread upma\n1 cup milk\n3 cashews\n4 almonds\n2 walnuts");
+                    tv_lunch2.setText("1 cup rajma curry\n1 cup spinach potato\n3 chapatti\n1/2 cup rice\nsalad");
+                    tv_dinner2.setText("1.5 cup parwal vegetable\n3 chapatti\nsalad");
                     break;
                 case 4:
-                    tv_breakfast2.setText("2 cucmber potato sandwich + 1 tsp green chutney + 1 orange juice + 3 cshews + 2 walnuts + 4 almonds");
-                    tv_lunch2.setText("1 cup white chana/ fish curry + 3 chapatti + 1/2 cup rice + salad");
-                    tv_dinner2.setText("1 cup cauliflower potato vegetable + 3 chapatti + salad");
+                    tv_breakfast2.setText("2 cucmber potato sandwich\n1 tsp green chutney\n1 orange juice\n3 cshews\n2 walnuts\n4 almonds");
+                    tv_lunch2.setText("1 cup white chana/ fish curry\n3 chapatti\n1/2 cup rice\nsalad");
+                    tv_dinner2.setText("1 cup cauliflower potato vegetable\n3 chapatti\nsalad");
                     break;
                 case 5:
-                    tv_breakfast2.setText("2 cup vegetable poha + 1 cup curd + 3 cashews + 4 almonds + 2 walnuts");
-                    tv_lunch2.setText("1 cup chana dal + 1 cup bhindi vegetable + 3 chapatti + 1/2 cup rice + salad");
-                    tv_dinner2.setText("1 cup peas mushroom vegetable + 3 chapatti + salad");
+                    tv_breakfast2.setText("2 cup vegetable poha\n1 cup curd\n3 cashews\n4 almonds\n2 walnuts");
+                    tv_lunch2.setText("1 cup chana dal\n1 cup bhindi vegetable\n3 chapatti\n1/2 cup rice\nsalad");
+                    tv_dinner2.setText("1 cup peas mushroom vegetable\n3 chapatti\nsalad");
                     break;
                 case 6:
-                    tv_breakfast2.setText("3 vegetable suji cheela + 1 cup strawberry shake + 4 cashews + 4 almonds + 3 walnuts");
-                    tv_lunch2.setText("1 cup mix dal + 1 cup soybean curry + 3 chapatti + 1/2 cup curd + salad");
-                    tv_dinner2.setText("1 cup karela vegetable + 3 chaptti + salad");
+                    tv_breakfast2.setText("3 vegetable suji cheela\n1 cup strawberry shake\n4 cashews\n4 almonds\n3 walnuts");
+                    tv_lunch2.setText("1 cup mix dal\n1 cup soybean curry\n3 chapatti\n1/2 cup curd\nsalad");
+                    tv_dinner2.setText("1 cup karela vegetable\n3 chaptti\nsalad");
                     break;
                 case 7:
-                    tv_breakfast2.setText("2 egg brown bread sandwich + green chutney + 1 cup milk + 3 cashews + 4 almonds + 2 walnuts");
-                    tv_lunch2.setText("1 cup arhar dal + 1 cup potato curry + 3 chapatti + 1/2 cup rice + 1/2 cup low fat curd + salad");
-                    tv_dinner2.setText("1.5 cup chicken curry + 3 chapatti + salad");
+                    tv_breakfast2.setText("2 egg brown bread sandwich\ngreen chutney\n1 cup milk\n3 cashews\n4 almonds\n2 walnuts");
+                    tv_lunch2.setText("1 cup arhar dal\n1 cup potato curry\n3 chapatti\n1/2 cup rice\n1/2 cup low fat curd\nsalad");
+                    tv_dinner2.setText("1.5 cup chicken curry\n3 chapatti\nsalad");
                     break;
             }
         }else if (bmi<25){
             tv_suggest.setText("Suggested calories per day: 2100-2300 kcal");
             switch (day) {
                 case 1:
-                    tv_breakfast2.setText("One grapefruit + Two poached eggs + Two slices whole-grain toast + One cup low-fat milk + One cup of black coffee");
-                    tv_lunch2.setText("Chicken breast (6-ounce portion) + salad + Glass of water");
-                    tv_dinner2.setText("One cup steamed broccoli + One cup of brown rice + Halibut + salad + One glass white wine + Sparkling water with lemon");
+                    tv_breakfast2.setText("One grapefruit\nTwo poached eggs\nTwo slices whole-grain toast\nOne cup low-fat milk\nOne cup of black coffee");
+                    tv_lunch2.setText("Chicken breast (6-ounce portion)\nsalad\nGlass of water");
+                    tv_dinner2.setText("One cup steamed broccoli\nOne cup of brown rice\nHalibut\nsalad\nOne glass white wine\nSparkling water with lemon");
                     break;
                 case 2:
-                    tv_breakfast2.setText("One whole-wheat English muffin + One orange + Large glass (12 ounces) non-fat milk + One cup of black coffee");
-                    tv_lunch2.setText("A turkey sandwich + one cup low-sodium vegetable soup + Glass of water");
-                    tv_dinner2.setText("Five-ounce sirloin steak + one mashed potatoes + one cup cooked spinach + One cup green beans + One glass beer + Sparkling water with lemon");
+                    tv_breakfast2.setText("One whole-wheat English muffin\nOne orange\nLarge glass (12 ounces) non-fat milk\nOne cup of black coffee");
+                    tv_lunch2.setText("A turkey sandwich\none cup low-sodium vegetable soup\nGlass of water");
+                    tv_dinner2.setText("Five-ounce sirloin steak\none mashed potatoes\none cup cooked spinach\nOne cup green beans\nOne glass beer\nSparkling water with lemon");
                     break;
                 case 3:
-                    tv_breakfast2.setText("One medium bran muffin + One serving turkey breakfast sausage + One orange + One cup non-fat milk + One cup black coffee");
-                    tv_lunch2.setText("Low sodium chicken noodle soup with six saltine crackers + One medium apple + Water");
-                    tv_dinner2.setText("8-ounce serving of turkey breast meat + One cup baked beans + One cup carrots + One cup kale + One glass of wine");
+                    tv_breakfast2.setText("One medium bran muffin\nOne serving turkey breakfast sausage\nOne orange\nOne cup non-fat milk\nOne cup black coffee");
+                    tv_lunch2.setText("Low sodium chicken noodle soup with six saltine crackers\nOne medium apple\nWater");
+                    tv_dinner2.setText("8-ounce serving of turkey breast meat\nOne cup baked beans\nOne cup carrots\nOne cup kale\nOne glass of wine");
                     break;
                 case 4:
-                    tv_breakfast2.setText("One cup whole wheat flakes + one cup non-fat milk + One banana + One slice whole-grain toast + One cup of black coffee");
-                    tv_lunch2.setText("Tuna wrap with one wheat flour tortilla + One sliced avocado + One cup non-fat milk");
-                    tv_dinner2.setText("One serving lasagna + Small garden salad with tomatoes and onions topped + One cup non-fat milk");
+                    tv_breakfast2.setText("One cup whole wheat flakes\none cup non-fat milk\nOne banana\nOne slice whole-grain toast\nOne cup of black coffee");
+                    tv_lunch2.setText("Tuna wrap with one wheat flour tortilla\nOne sliced avocado\nOne cup non-fat milk");
+                    tv_dinner2.setText("One serving lasagna\nSmall garden salad with tomatoes and onions topped\nOne cup non-fat milk");
                     break;
                 case 5:
-                    tv_breakfast2.setText("One piece of French toast + One scrambled + One serving turkey bacon + One cup orange juice + One cup black coffee");
-                    tv_lunch2.setText("Veggie burger on a whole grain bun + One cup northern beans + One cup non-fat milk");
-                    tv_dinner2.setText("One trout filet + One cup green beans + One cup brown rice + One small garden salad + One glass of beer + Sparkling water with lemon");
+                    tv_breakfast2.setText("One piece of French toast\nOne scrambled\nOne serving turkey bacon\nOne cup orange juice\nOne cup black coffee");
+                    tv_lunch2.setText("Veggie burger on a whole grain bun\nOne cup northern beans\nOne cup non-fat milk");
+                    tv_dinner2.setText("One trout filet\nOne cup green beans\nOne cup brown rice\nOne small garden salad\nOne glass of beer\nSparkling water with lemon");
                     break;
                 case 6:
-                    tv_breakfast2.setText("One cup corn flakes + one cup non-fat milk + One banana + One hard-boiled egg + One cup black coffee");
-                    tv_lunch2.setText("One cup whole wheat pasta + Medium garden salad + Glass of water");
-                    tv_dinner2.setText("Four and one-half ounce serving of pork loin + Small garden salad + One small baked sweet potato + One cup asparagus + One glass wine + Sparkling water with lemon");
+                    tv_breakfast2.setText("One cup corn flakes\none cup non-fat milk\nOne banana\nOne hard-boiled egg\nOne cup black coffee");
+                    tv_lunch2.setText("One cup whole wheat pasta\nMedium garden salad\nGlass of water");
+                    tv_dinner2.setText("Four and one-half ounce serving of pork loin\nSmall garden salad\nOne small baked sweet potato\nOne cup asparagus\nOne glass wine\nSparkling water with lemon");
                     break;
                 case 7:
-                    tv_breakfast2.setText("One cup oatmeal + one-half cup non-fat milk + Two slices turkey bacon + One cup non-fat milk + One cup black coffee");
-                    tv_lunch2.setText("Six-ounce baked chicken breast + Large garden salad + One baked sweet potato + One whole-wheat dinner roll + Glass of water");
-                    tv_dinner2.setText("3-ounce serving of baked salmon + One-half cup black beans + One cup Swiss chard + One cup brown rice + Sparkling water + One whole wheat dinner roll");
+                    tv_breakfast2.setText("One cup oatmeal\none-half cup non-fat milk \nTwo slices turkey bacon \nOne cup non-fat milk \nOne cup black coffee");
+                    tv_lunch2.setText("Six-ounce baked chicken breast \nLarge garden salad \nOne baked sweet potato \nOne whole-wheat dinner roll \nGlass of water");
+                    tv_dinner2.setText("3-ounce serving of baked salmon \nOne-half cup black beans \nOne cup Swiss chard \nOne cup brown rice \nSparkling water \nOne whole wheat dinner roll");
                     break;
             }
         }else {
             tv_suggest.setText("Suggested calories per day: 1600 kcal");
             switch (day) {
                 case 1:
-                    tv_breakfast2.setText("1 onion stuffed chapatti + 1/2 cup low fat curd");
-                    tv_lunch2.setText("1 cup moong dal/ chicken curry + 1 chapatti + salad");
-                    tv_dinner2.setText("1 cup beans + 1 chapatti + salad");
+                    tv_breakfast2.setText("1 onion stuffed chapatti\n1/2 cup low fat curd");
+                    tv_lunch2.setText("1 cup moong dal/ chicken curry\n1 chapatti\nsalad");
+                    tv_dinner2.setText("1 cup beans\n1 chapatti\nsalad");
                     break;
                 case 2:
-                    tv_breakfast2.setText("2 besan cheela + 1/2 cup low fat curd");
-                    tv_lunch2.setText("1 cup masoor dal + 1 chapatti + 1/2 up low fat curd + salad");
-                    tv_dinner2.setText("1 cup carrot peas vegetable +1 chapatti + salad");
+                    tv_breakfast2.setText("2 besan cheela\n1/2 cup low fat curd");
+                    tv_lunch2.setText("1 cup masoor dal\n1 chapatti\n1/2 up low fat curd\nsalad");
+                    tv_dinner2.setText("1 cup carrot peas vegetable +1 chapatti\nsalad");
                     break;
                 case 3:
-                    tv_breakfast2.setText("1 cup vegetable brown bread upma + 1/2 cup low fat milk (no sugar)");
-                    tv_lunch2.setText("1 cup rajma curry + 1 chapatti + salad");
-                    tv_dinner2.setText("1 cup parwal vegetable + 1 chapatti + salad");
+                    tv_breakfast2.setText("1 cup vegetable brown bread upma\n1/2 cup low fat milk (no sugar)");
+                    tv_lunch2.setText("1 cup rajma curry\n1 chapatti\nsalad");
+                    tv_dinner2.setText("1 cup parwal vegetable\n1 chapatti\nsalad");
                     break;
                 case 4:
-                    tv_breakfast2.setText("1 cucmber hungcurd sandwich + 1/2 tsp green chutney + 1 orange");
-                    tv_lunch2.setText("1 cup white chana/ fish curry + 1 chapatti + salad");
-                    tv_dinner2.setText("1 cup cauliflower vegetable + 1 chapatti + salad");
+                    tv_breakfast2.setText("1 cucmber hungcurd sandwich\n1/2 tsp green chutney\n1 orange");
+                    tv_lunch2.setText("1 cup white chana/ fish curry\n1 chapatti\nsalad");
+                    tv_dinner2.setText("1 cup cauliflower vegetable\n1 chapatti\nsalad");
                     break;
                 case 5:
-                    tv_breakfast2.setText("1 cup vegetable poha + 1 cup low fat curd");
-                    tv_lunch2.setText("1 cup chana dal + 1 chapatti + salad");
-                    tv_dinner2.setText("1 cup tinda vegetable + 1 chapatti + salad");
+                    tv_breakfast2.setText("1 cup vegetable poha\n1 cup low fat curd");
+                    tv_lunch2.setText("1 cup chana dal\n1 chapatti\nsalad");
+                    tv_dinner2.setText("1 cup tinda vegetable\n1 chapatti\nsalad");
                     break;
                 case 6:
-                    tv_breakfast2.setText("1 cup low fat milk with oats + 3-4 strawberries");
-                    tv_lunch2.setText("1 cup soybean curry + 1 chapatti + 1/2 cup low fat curd + salad");
-                    tv_dinner2.setText("1 cup ghia vegetable + 1 chaptti + salad");
+                    tv_breakfast2.setText("1 cup low fat milk with oats\n3-4 strawberries");
+                    tv_lunch2.setText("1 cup soybean curry\n1 chapatti\n1/2 cup low fat curd\nsalad");
+                    tv_dinner2.setText("1 cup ghia vegetable\n1 chaptti\nsalad");
                     break;
                 case 7:
-                    tv_breakfast2.setText("3 egg whites + 1 toasted brown bread + 1/2 cup low fat milk (no sugar)");
-                    tv_lunch2.setText("1 cup arhar dal + 1 chapatti + 1/2 cup low fat curd + salad");
-                    tv_dinner2.setText("1 cup pumpkin + 1 chapatti + salad");
+                    tv_breakfast2.setText("3 egg whites\n1 toasted brown bread\n1/2 cup low fat milk (no sugar)");
+                    tv_lunch2.setText("1 cup arhar dal\n1 chapatti\n1/2 cup low fat curd\nsalad");
+                    tv_dinner2.setText("1 cup pumpkin\n1 chapatti\nsalad");
                     break;
             }
         }
