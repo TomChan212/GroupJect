578f7079070c63d6159f5347ac411c305758a119
Lizeris <j.l.jonasleung@gmail.com>
2022-04-26 15:28:16 +0800

    Progress bar can be change

--- app/src/main/java/pk/name/myapplication/progressFragment.java
+++ app/src/main/java/pk/name/myapplication/progressFragment.java
@@ -1,5 +1,7 @@
 package pk.name.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import pk.name.myapplication.databinding.FragmentHomeBinding;
 import pk.name.myapplication.databinding.FragmentProgressBinding;
@@ -17,16 +21,38 @@ public class progressFragment extends Fragment implements View.OnClickListener {
     private FragmentProgressBinding binding;
 
     Button progress_back;
+    //For progress day
+    private TextView tv_day;
+    private ProgressBar mProgress;
+    SharedPreferences sharedPreferences;
+    public static final String sday = "sdayKey";
+    public static final String mypref = "mypref";
+    private int day;
+    //end of progress day
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //for button back
         binding = FragmentProgressBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        // Inflate the layout for this fragment
+        View root = binding.getRoot();        // Inflate the layout for this fragment
+
+        sharedPreferences = getActivity().getSharedPreferences(mypref, Context.MODE_PRIVATE);
+        tv_day = root.findViewById(R.id.progress_count);
+        mProgress = root.findViewById(R.id.progress_bar);
+        day = sharedPreferences.getInt(sday,1);
+        tv_day.setText("Day: "+String.valueOf(day)+"/7");
+        mProgress.setProgress(day);
+
+
+
+
+
+
 
         progress_back = root.findViewById(R.id.progress_back);
         progress_back.setOnClickListener(this);
-
+        //end of button back
         return root;
     }
 
